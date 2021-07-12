202c4fc0b54d2dfc6917e07400ab211794af1dc3
Hirdesh Kumar <79218874+hirdeshkumar2407@users.noreply.github.com>
2021-07-12 12:38:35 +0500

    Add files via upload

---
+++ modeling mongosee/oneToManyFew case1/server.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const db = require("./models");
+
+const createTutorial = function(tutorial) {
+  return db.Tutorial.create(tutorial).then(docTutorial => {
+    console.log("\n>> Created Tutorial:\n", docTutorial);
+    return docTutorial;
+  });
+};
+
+const createImage = function(tutorialId, image) {
+  console.log("\n>> Add Image:\n", image);
+  return db.Tutorial.findByIdAndUpdate(
+    tutorialId,
+    {
+      $push: {
+        images: {
+          url: image.url,
+          caption: image.caption
+        }
+      }
+    },
+    { new: true, useFindAndModify: false }
+  );
+};
+
+const run = async function() {
+  var tutorial = await createTutorial({
+    title: "Tutorial #1",
+    author: "bezkoder"
+  });
+
+  tutorial = await createImage(tutorial._id, {
+    path: "sites/uploads/images/mongodb.png",
+    url: "/images/mongodb.png",
+    caption: "MongoDB Database",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  tutorial = await createImage(tutorial._id, {
+    path: "sites/uploads/images/one-to-many.png",
+    url: "/images/one-to-many.png",
+    caption: "One to Many Relationship",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+};
+
+mongoose
+  .connect("mongodb://localhost/bez", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch(err => console.error("Connection error", err));
+
+run();
---
+++ modeling mongosee/oneToOne E/server.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/oneToOneE", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch(err => console.error("Connection error", err));
+
+
+
+
+
+
+
+
+const Customer = require("./models/Customer").Customer;
+const Identifier = require("./models/Identifier");
+
+const createCustomer = function(name, age, gender) {
+  const customer = new Customer({
+    name,
+    age,
+    gender
+  });
+
+  return customer.save();
+};
+
+const createIdentifier = function(cardCode, customer) {
+  const identifier = new Identifier({
+    cardCode,
+    customer
+  });
+
+  return identifier.save();
+};
+
+createCustomer("bezkoder", 29, "male")
+  .then(customer => {
+    console.log("> Created new Customer\n", customer);
+
+    return createIdentifier(
+      customer._id.toString().substring(0, 10).toUpperCase(),
+      customer
+    );
+  })
+  .then(identifier => {
+    console.log("> Created new Identifier\n", identifier);
+  })
+  .catch(err => console.log(err));
